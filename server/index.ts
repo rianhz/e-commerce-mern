4e77839365e51e8e5f0ad30e87e5a213930365f5
rianhidayat <ryanhidayat.rh197@gmail.com>
2023-04-09 11:17:06 +0800

    .

--- server/index.ts
+++ server/index.ts
@@ -12,8 +12,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(
 	cors({
-		origin: "https://e-commerce-fe-five.vercel.app",
+		origin: "https://e-commerce-fe-five.vercel.app/",
 		credentials: true,
+		allowedHeaders: "*",
 	})
 );
 app.use(express.json());
