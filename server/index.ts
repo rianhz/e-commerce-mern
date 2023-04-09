bf19b919280711982998359be8aa18a1e1802cf2
rianhidayat <ryanhidayat.rh197@gmail.com>
2023-04-09 11:47:00 +0800

    .

--- server/index.ts
+++ server/index.ts
@@ -17,6 +17,14 @@ app.use(
 		allowedHeaders: "*",
 	})
 );
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
 app.use(express.json());
 app.use(cookieParser());
 app.use("/images", express.static(path.join(__dirname, "images")));
